1e79acf2f4e85c6b22d68d656f562e745b8b092e
obsoleter <jeremy@obsoleter.com>
2012-05-30 03:28:25 -0400

    Add many things. Should have made more commits.

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ import os
 import json
 import uuid
 import stat
+from datetime import timedelta, datetime
+from functools import wraps
 from wand.image import Image
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash, send_from_directory
 from werkzeug import secure_filename
 from flask.ext.sqlalchemy import SQLAlchemy
 from flaskext.bcrypt import Bcrypt
-from flaskext.wtf import Form, TextField, PasswordField, BooleanField, validators, RecaptchaField
+from flaskext.wtf import Form, TextField, PasswordField, BooleanField, \
+    Required, EqualTo, RecaptchaField, ValidationError
+from flask.ext.login import (LoginManager, current_user, login_required,
+                            login_user, logout_user, UserMixin, AnonymousUser,
+                            confirm_login, fresh_login_required)
+from wtforms.ext.dateutil.fields import DateField
 
 DEBUG = True
 UUID4_RE = re.compile(
@@ -29,19 +36,27 @@ RECAPTCHA_USE_SSL = True
 RECAPTCHA_PUBLIC_KEY = '6Lc9EdISAAAAAEVtUpivYYV-xgzGtfrNGrTYfhwe' 
 RECAPTCHA_PRIVATE_KEY = '6Lc9EdISAAAAACaPQmjh_rUUAldVoAhfZKydZrAQ'
 
+PERMANENT_SESSION_LIFETIME = timedelta(days=180)
+
 app = Flask(__name__)
 app.config.from_object(__name__)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.anonymous_user = AnonymousUser
+login_manager.login_view = 'login'
+login_manager.login_message = u'Please log in to access this page'
 
 class User(db.Model):
     __tablename__ = 'users'
-    uid = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(60))
     pwdhash = db.Column(db.String())
     email = db.Column(db.String(60))
     activate = db.Column(db.Boolean)
     created = db.Column(db.DateTime)
+    events = db.relationship('Event', backref='user', lazy='dynamic')
 
     def __init__(self, username, password, email):
         self.username = username
@@ -56,17 +71,55 @@ class User(db.Model):
     def check_password(self, password):
         return bcrypt.check_password_hash(self.pwdhash, password)
 
+    def get_id(self):
+        return unicode(self.id)
+
+    def is_active(self):
+        # TODO: Remove True once activation is configured
+        return True or self.activate
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(100))
+    date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Event %r>' % self.username
+
+    def __init__(self, user_id, name, date):
+        self.user_id = user_id
+        self.name = name
+        self.date = date
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
+    
+login_manager.setup_app(app)
+
 class SignupForm(Form):
-    username = TextField('Username', [validators.Required()])
-    password = PasswordField('Password', [validators.Required(), validators.EqualTo('confirm', message='Passwords must match')])
-    confirm = PasswordField('Confirm Password', [validators.Required()])
-    email = TextField('eMail', [validators.Required()])
-    accept_tos = BooleanField('I accept the TOS', [validators.Required])
+    username = TextField('Username', validators=[Required()])
+    password = PasswordField('Password', validators=[Required(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password', validators=[Required()])
+    email = TextField('Email', validators=[Required()])
+    accept_tos = BooleanField('I accept the Terms of Service', validators=[Required()])
     recaptcha = RecaptchaField()
 
 class LoginForm(Form):
-    username = TextField('Username', [validators.Required()])
-    password = TextField('Password', [validators.Required()])
+    username = TextField('Username')
+    password = PasswordField('Password')
+
+class EventForm(Form):
+    name = TextField('Event name', validators=[Required()])
+    date = DateField('Date', validators=[Required()])
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -102,12 +155,64 @@ def is_uuid_file(file):
     name = file.split('.')[0]
     return True if UUID4_RE.match(name) else False
 
+def not_logged_in(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if current_user.is_authenticated():
+            return redirect(url_for('home'))
+        else:
+            return f(*args, **kwargs)
+    return wrapper
+    
+@app.route('/login/', methods=['GET', 'POST'])
+@not_logged_in
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user or not user.check_password(form.password.data):
+            form.errors['username'] = [u'Incorrect username or password.']
+        elif login_user(user, remember=True):
+            return redirect(request.args.get('next') or url_for('home'))
+    return render_template('login.html', form=form)
+
 @app.route("/signup/", methods=['GET', 'POST'])
+@not_logged_in
 def signup():
     form = SignupForm()
+    if form.validate_on_submit():
+        user = User(form.username.data, form.password.data, form.email.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash('Your account was created successfully!')
+        return redirect(url_for('home'))
     return render_template('signup.html', form=form)
 
-#user = User.query.filter_by(username=form.username)
+@app.route('/events/', methods=['GET', 'POST'])
+@login_required
+def events():
+    form = EventForm()
+    if form.validate_on_submit():
+        event = Event(current_user.id, form.name.data, form.date.data)
+        db.session.add(event)
+        db.session.commit()
+        flash('Your event was created successfully!')
+        return redirect(url_for('home'))
+    return render_template('event.html', form=form)
+
+@app.route("/home/")
+@login_required
+def home():
+    return render_template('home.html', user=current_user)
+
+@app.route('/events/<event>')
+def event():
+    return ''
+
+@app.route('/photos/<event>')
+def photos():
+    return ''
 
 @app.route("/upload/", methods=['GET', 'POST'])
 def upload():
