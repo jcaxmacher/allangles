f73c2377fbcbf3b8313cd4abae7ef8abc416ddf5
obsoleter <jeremy@obsoleter.com>
2012-05-29 23:54:52 -0400

    Break up templates. Add signup template. Add recaptcha for signup.

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
 ALLOWED_EXTENSIONS = set(['jpg', 'png', 'gif'])
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(os.getcwd(), 'dev.db')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'j2\xa9\xb7\xe4\xe4D\t\xc0\x052q\xfc\x8c\xe9\xbe\x0c\x0e#:\x9f\xc5\xa9\xcf'
+)
+
+RECAPTCHA_USE_SSL = True
+RECAPTCHA_PUBLIC_KEY = '6Lc9EdISAAAAAEVtUpivYYV-xgzGtfrNGrTYfhwe' 
+RECAPTCHA_PRIVATE_KEY = '6Lc9EdISAAAAACaPQmjh_rUUAldVoAhfZKydZrAQ'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
